df20bd33e1fd6218d1250235ba87106731def95a
lproductions <l.productionsuk@gmail.com>
2014-12-07 20:09:06 +0000

    1

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace WindowsFormsApplication1 {
+    public partial class Form1 : Form {
+        string Health = "Health : 10                    ";
+        string Armour = "Armour : 10                    ";
+        string Coordinates = "Location: 8,8                    ";
+        int xcoordinate = 8;
+        int ycoordinate = 8;
+        bool namepicked = false;
+        bool ready = false;
+        string name;
+        string namedisplay;
+        string story;
+        double temp= 35.0D;
+        bool day = true;
+        bool ismoved = false;
+        area[] land;
+        System.Timers.Timer aTimer;
+
+        public Form1() {
+            Random r1 = new Random(DateTime.Now.Millisecond * DateTime.Now.Minute + DateTime.Now.Month / DateTime.Now.Hour * DateTime.Now.Second);
+            land = new area[256];
+            for (int i = 0; i < 255;i++ ) {
+                land[i] = new area(r1);
+            }
+            InitializeComponent();
+            textBox2.KeyDown += new KeyEventHandler(textBox2_KeyPress);
+            richTextBox1.Text = "What is though Adventures Name";
+
+
+
+        }
+
+        public void command(string text123) {
+            if (namepicked == true) {
+                if (ready) {
+                    if (text123.ToLower().Equals("map")) {
+
+
+                        mainsetup();
+
+
+
+                    }
+                }
+                else {
+                    if (text123.ToLower().Equals("yes")) {
+                        richTextBox1.Text = "Lets Go";
+                        Task.Delay(3500).Wait();
+                        mainsetup();
+                       
+                        aTimer = new System.Timers.Timer(500);
+                        // Hook up the Elapsed event for the timer. 
+                        aTimer.Elapsed += halfsecondupdate;
+                        aTimer.Enabled = true;
+                    }
+
+                }
+
+
+            }
+
+
+            else {
+                name = text123;
+                namedisplay = "Name: " + text123 + "                    ";
+                story = "Now " + name + " I am the voice you will hear in your head every now and then" + Environment.NewLine +
+                        "You have been Cast under a Wizards spell so you cannot leave this area" + Environment.NewLine +
+                        "To Get Back to your home you must defeat this evil Wizard " + Environment.NewLine +
+                        "Unfortunatly you are under the control of the nearest person i could find so Are You Ready?";
+                richTextBox1.Text = story;
+
+
+               
+                story = null;
+                land[127].explored = true;
+                namepicked = true;
+                
+
+
+                
+            }
+            textBox2.Text = "";
+        }
+
+         
+       
+        void textBox2_KeyPress(object sender, System.Windows.Forms.KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+
+                command(textBox2.Text);
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+
+            }
+
+
+
+        }
+        public void setTextBox(string value) {
+            if (InvokeRequired) {
+                this.Invoke(new Action<string>(setTextBox), new object[] { value });
+                return;
+            }
+            richTextBox1.Text = value;
+        }
+        public void callmainupdate() {
+            if (InvokeRequired) {
+                this.Invoke(new Action(callmainupdate));
+                return;
+            }
+            mainsetup();
+        }
+
+
+
+        void mainsetup() {
+            if (!ismoved) {
+                setTextBox(namedisplay + Health + Armour + Coordinates + ( Math.Truncate(temp * 100) / 100) + "C                    " +  
+                    Environment.NewLine + Environment.NewLine + Environment.NewLine + "you spawn in " + land[xcoordinate + (ycoordinate * 16)].description);
+            }
+            if (ismoved) {
+                setTextBox(namedisplay + Health + Armour + Coordinates + (Math.Truncate(temp * 100) / 100) + "C                    " +  
+                    Environment.NewLine + Environment.NewLine + Environment.NewLine + "you are in  " + land[xcoordinate + (ycoordinate * 16)].description);
+            }
+           
+            //name
+            richTextBox1.Select(0, name.Length - 1);
+            richTextBox1.SelectionColor = Color.Black;
+            richTextBox1.DeselectAll();
+           
+
+            //health
+            richTextBox1.Select(richTextBox1.Text.IndexOf(Health), Health.Length - 1);
+            richTextBox1.SelectionColor = Color.Red;
+            richTextBox1.DeselectAll();
+            richTextBox1.SelectionColor = Color.Empty;
+            //armour
+
+
+            richTextBox1.DeselectAll();
+            richTextBox1.Select(richTextBox1.Text.IndexOf(Armour), richTextBox1.Text.IndexOf(Armour) - 1);
+            richTextBox1.SelectionColor = Color.Blue;
+
+
+            //location
+
+            richTextBox1.DeselectAll();
+            richTextBox1.Select(richTextBox1.Text.IndexOf(Coordinates), richTextBox1.Text.IndexOf(Coordinates) + Coordinates.Length - 1);
+            richTextBox1.SelectionColor = Color.Black;
+
+            //temp
+            richTextBox1.DeselectAll();
+            richTextBox1.Select(richTextBox1.Text.IndexOf((Math.Truncate(temp * 100) / 100).ToString()), richTextBox1.Text.IndexOf((Math.Truncate(temp * 100) / 100).ToString()) + (Math.Truncate(temp * 100) / 100).ToString().Length - 1);
+            if (temp > 34) {
+                richTextBox1.SelectionColor = Color.Red;
+            }
+            
+            if (temp < 34) {
+                richTextBox1.SelectionColor = Color.Blue;
+            }
+            richTextBox1.DeselectAll();
+
+
+            richTextBox1.Select(richTextBox1.Text.IndexOf((Math.Truncate(temp * 100) / 100).ToString()) + (Math.Truncate(temp * 100) / 100).ToString().Length + 1, richTextBox1.Text.Length);
+            richTextBox1.SelectionColor = Color.Black;
+            namepicked = true;
+            
+        }
+
+
+        void halfsecondupdate(Object source, ElapsedEventArgs e) {
+            callmainupdate();
+            if (day) {
+                if (land[xcoordinate + (ycoordinate * 16)].weather == 0) {
+                    temp += 0.01;
+                }
+                else if (land[xcoordinate + (ycoordinate * 16)].weather == 1) {
+                    temp -= 0.005;
+                }
+                else if (land[xcoordinate + (ycoordinate * 16)].weather == 2) {
+                    temp -= 0.02;
+                }
+            }
+            else {
+
+                if (land[xcoordinate + (ycoordinate * 16)].weather == 0) {
+                    temp -= 0.005;
+                }
+                else if (land[xcoordinate + (ycoordinate * 16)].weather == 1) {
+                    temp -= 0.025;
+                }
+                else if (land[xcoordinate + (ycoordinate * 16)].weather == 2) {
+                    temp -= 0.10;
+                }
+
+            }
+            Console.Out.WriteLine(Math.Truncate(temp));
+        }
+    }
+}
---
+++ WindowsFormsApplication1/area.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1 {
+    class area {
+       public byte animal;
+       public short biome;
+       public byte environment1;
+       public byte environment2;
+       public byte weather;
+       public bool explored = false;
+       public string description = "";
+       static int test = 0;
+        public area(Random r) {
+             
+            animal = (byte)r.Next(0, 7);
+            biome = (short)r.Next(0, 11);
+            environment1 = (byte)r.Next(0, 11);
+            environment2 = (byte)r.Next(0, 11);
+            weather = (byte)r.Next(0, 3);
+            
+            // description
+
+            if(biome ==0){
+                description = "a barren wasteland ";
+                animal = 0;
+                
+            }
+            else if(biome==1){
+                description = "a luxurious greenland";
+                if(animal == 0){
+                animal =(byte) r.Next(1,9);
+                if (animal == 7 | animal == 8) {
+                    Console.Out.WriteLine(animal);
+                    animal = 6;
+                }
+                }
+
+
+            }
+            else if (biome == 2) {
+                description = "a mountainous region";
+            }
+            else if (biome == 3) {
+                description = "a barren wasteland ";
+            }
+            if(animal == 0){
+
+
+            }
+            else if (animal == 1) {
+                description += "  with some wooly sheep";
+
+            }
+            else if (animal == 2) {
+                description += " with some scary Bears";
+
+            }
+            else if (animal == 3) {
+                description += "  with some Horses";
+
+            }
+            else if (animal == 4) {
+                description += "  with some Pigs";
+
+            }
+            else if (animal == 5) {
+
+                description += "  with some Cows";
+            }
+            else if (animal == 6) {
+
+                description += "with some Pigs, Cows And Sheep lucky you....i'll go away in jealousy now";
+            }
+            Console.Out.WriteLine(description);
+        }
+    }
+}
