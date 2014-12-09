8fa51bedad7d0d1d3d57cfa0612590306627b183
lproductions <l.productionsuk@gmail.com>
2014-12-09 22:46:14 +0000

    ysrghfdgh

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -27,6 +27,10 @@ namespace WindowsFormsApplication1 {
         bool isviewingmap = false;
         string temperture = "35.0C                    ";
        string maptext = "";
+        bool deniednorth = false;
+        bool deniedsouth = false;
+        bool deniedwest = false;
+        bool deniedeast = false;
         area[] land;
         System.Timers.Timer aTimer;
 
@@ -37,6 +41,7 @@ namespace WindowsFormsApplication1 {
                 land[i] = new area(r1);
             }
             InitializeComponent();
+            richTextBox1.Font = new Font(FontFamily.GenericMonospace, richTextBox1.Font.Size);
             textBox2.KeyDown += new KeyEventHandler(textBox2_KeyPress);
             richTextBox1.Text = "What is thou Adventures Name";
 
@@ -73,6 +78,83 @@ namespace WindowsFormsApplication1 {
                             }
                         
 
+                    }
+                    else if (text123.ToLower().Equals("move up") | text123.ToLower().Equals("move north") | text123.ToLower().Equals("up")) {
+                        if (ycoordinate != 0) {
+                            ycoordinate--;
+                            Coordinates ="Location: " + xcoordinate + "," +ycoordinate + "                   ";
+                            ismoved = true;
+                            callmainupdate();
+                            deniednorth = false;
+                            deniedeast = false;
+                            deniedsouth = false;
+                            deniedwest = false;
+                            land[xcoordinate + (ycoordinate * 32)].explored = true;
+                        }
+                        else {
+                            callmainupdate();
+                            deniednorth = true;
+                        }
+                        isviewingmap = false;
+                    }
+
+                        //moving
+                   
+                    else if (text123.ToLower().Equals("move left") | text123.ToLower().Equals("move west") | text123.ToLower().Equals("left")) {
+                        if (xcoordinate != 0) {
+                            xcoordinate--;
+                            Coordinates = "Location: " + xcoordinate + "," + ycoordinate + "                   ";
+                            ismoved = true;
+                            callmainupdate();
+                            deniednorth = false;
+                            deniedeast = false;
+                            deniedsouth = false;
+                            deniedwest = false;
+                            land[xcoordinate + (ycoordinate * 32)].explored = true;
+                        }
+                        else {
+                            callmainupdate();
+                            deniedwest = true;
+                        }
+                        isviewingmap = false;
+                    }
+                    else if (text123.ToLower().Equals("move down") | text123.ToLower().Equals("move south") | text123.ToLower().Equals("down")) {
+                        if (ycoordinate != 7) {
+                            ycoordinate++;
+                            Coordinates = "Location: " + xcoordinate + "," + ycoordinate + "                   ";
+                            ismoved = true;
+                            callmainupdate();
+                            deniednorth = false;
+                            deniedeast = false;
+                            deniedsouth = false;
+                            deniedwest = false;
+                            land[xcoordinate + (ycoordinate * 32)].explored = true;
+                        }
+                        else {
+                            callmainupdate();
+                            deniedsouth = true;
+                        }
+                        isviewingmap = false;
+                    }
+                    else if (text123.ToLower().Equals("move right") | text123.ToLower().Equals("move east") | text123.ToLower().Equals("right")) {
+                        if (xcoordinate != 31) {
+                            xcoordinate++;
+                            Coordinates = "Location: " + xcoordinate + "," + ycoordinate + "                   ";
+                            ismoved = true;
+
+                            deniednorth = false;
+                            deniedeast = false;
+                            deniedsouth = false;
+                            deniedwest = false;
+                            land[xcoordinate + (ycoordinate * 32)].explored = true;
+                            callmainupdate();
+                        }
+                        else {
+                            
+                            deniedeast = true;
+                            callmainupdate();
+                        }
+                        isviewingmap = false;
                     }
 
 
@@ -142,6 +224,13 @@ namespace WindowsFormsApplication1 {
             }
             richTextBox1.Text = value;
         }
+        public void addTextBox(string value) {
+            if (InvokeRequired) {
+                this.Invoke(new Action<string>(setTextBox), new object[] { value });
+                return;
+            }
+            richTextBox1.Text += value;
+        }
         public void callmainupdate() {
             if (InvokeRequired) {
                 this.Invoke(new Action(callmainupdate));
@@ -158,7 +247,19 @@ namespace WindowsFormsApplication1 {
                 if (!isviewingmap) {
                 setTextBox(namedisplay + Health + Armour + Coordinates + temperture+  
                     Environment.NewLine + Environment.NewLine + Environment.NewLine + "you spawn in " +
-                    land[xcoordinate + (ycoordinate * 16)].description);
+                    land[xcoordinate + (ycoordinate * 16)].description + ".");
+                if (deniednorth) {
+                    addTextBox(Environment.NewLine + "You cannot move further north");
+                }
+                if (deniedsouth) {
+                    addTextBox(Environment.NewLine + "You cannot move further south");
+                }
+                if (deniedwest) {
+                    addTextBox(Environment.NewLine + "You cannot move further west");
+                }
+                if (deniedeast) {
+                    addTextBox(Environment.NewLine + "You cannot move further east");
+                }
                 }
                 else {
                      setTextBox(namedisplay + Health + Armour + Coordinates + temperture+  
@@ -169,7 +270,19 @@ namespace WindowsFormsApplication1 {
                 if (!isviewingmap) {
                     setTextBox(namedisplay + Health + Armour + Coordinates + temperture +
                         Environment.NewLine + Environment.NewLine + Environment.NewLine + "you are in " +
-                        land[xcoordinate + (ycoordinate * 16)].description);
+                        land[xcoordinate + (ycoordinate * 16)].description + ".");
+                    if (deniednorth) {
+                        addTextBox(Environment.NewLine + "You cannot move further north");
+                    }
+                    if (deniedsouth) {
+                        addTextBox(Environment.NewLine + "You cannot move further south");
+                    }
+                    if (deniedwest) {
+                        addTextBox(Environment.NewLine + "You cannot move further west");
+                    }
+                    if (deniedeast) {
+                        addTextBox(Environment.NewLine + "You cannot move further east");
+                    }
                 }
                 else {
                     setTextBox(namedisplay + Health + Armour + Coordinates + temperture +
